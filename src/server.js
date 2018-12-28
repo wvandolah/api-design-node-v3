33574c3ccc7c1562976be1bfcc605795e5136bcd
William VanDolah <william.vandolah@gmail.com>
2018-12-28 10:33:34 -0600

    lesson 1 done

--- src/server.js
+++ src/server.js
@@ -12,4 +12,16 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-export const start = () => {}
+app.get('/', (req, res) => {
+  res.send({ message: 'hello' })
+})
+
+app.post('/', (req, res) => {
+  console.log(req.body)
+  res.send({ message: 'ok' })
+})
+export const start = () => {
+  app.listen(3000, () => {
+    console.log('server is on 3000')
+  })
+}
